af18c2346c47d9ce01ed093695ce5bad36b720e4
dbu <david.buchmann@liip.ch>
2010-10-26 11:37:35 +0200

    fixing empty suites to be suites instead of cases

--- tests/read/LockTest.php
+++ tests/read/LockTest.php
@@ -1,6 +1,6 @@
 <?php
-require_once(dirname(__FILE__) . '/../../inc/baseCase.php');
+require_once(dirname(__FILE__) . '/../../inc/baseSuite.php');
 
-class jackalope_tests_read_LockTest extends jackalope_baseCase {
+class jackalope_tests_read_LockTest extends jackalope_baseSuite {
     //todo: javax.jcr.lock
 }
--- tests/read/QueryObjectModelTest.php
+++ tests/read/QueryObjectModelTest.php
@@ -1,6 +1,6 @@
 <?php
-require_once(dirname(__FILE__) . '/../../inc/baseCase.php');
+require_once(dirname(__FILE__) . '/../../inc/baseSuite.php');
 
-class jackalope_tests_read_QueryObjectModelTest extends jackalope_baseCase {
+class jackalope_tests_read_QueryObjectModelTest extends jackalope_baseSuite {
     
 }
