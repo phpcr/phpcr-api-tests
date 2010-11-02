d55747a7fc3b0920c8ac50f3211dc36a57d0e822
dbu <david.buchmann@liip.ch>
2010-11-02 12:46:09 +0100

    testing binary stuff

--- tests/read/ReadTest/BinaryReadMethods.php
+++ tests/read/ReadTest/BinaryReadMethods.php
@@ -6,6 +6,7 @@ require_once(dirname(__FILE__) . '/../../../inc/baseCase.php');
 class jackalope_tests_read_ReadTest_BinaryReadMethods extends jackalope_baseCase {
     protected $node;
     public $binary;
+    private $binarystring = 'aDEuIENoYXB0ZXIgMSBUaXRsZQoKCiogZm9vCiogYmFyCioqIGZvbzIKKiogZm9vMwoqIGZvbzAKCnx8IGhlYWRlciB8fCBiYXIgfHwKfCBoIHwgaiB8IAoKW0Zvb3wgaHR0cDovL2xpaXAuY2hdCgp7Y29kZX0KaGVsbG8gd29ybGQKe2NvZGV9CgojIGZvbwojIGIKCgpoMi4gU2VjdGlvbiAxLjEgVGl0bGUKCgpTdWJzZWN0aW9uIDEuMS4xIFRpdGxlCn5+fn5+fn5+fn5+fn5+fn5+fn5+fn4KClNlY3Rpb24gMS4yIFRpdGxlCi0tLS0tLS0tLS0tLS0tLS0tCgpDaGFwdGVyIDIgVGl0bGUKPT09PT09PT09PT09PT09Cg==';
 
     static public function  setupBeforeClass() {
         parent::setupBeforeClass();
@@ -38,9 +39,8 @@ class jackalope_tests_read_ReadTest_BinaryReadMethods extends jackalope_baseCase
     public function testGetStream() {
         $stream = $this->binary->getStream();
         $this->assertNotNull($stream);
-        $this->markTestIncomplete('TODO: what is a stream here?');
-        //var_dump($stream);
-        //echo file_get_contents($stream);
+        $bytes = fread($stream, $this->binary->getSize());
+        $this->assertEquals($this->binarystring, $bytes);
     }
 
     /** @expectedException PHPCR_BadMethodCallException */
@@ -53,7 +53,7 @@ class jackalope_tests_read_ReadTest_BinaryReadMethods extends jackalope_baseCase
         $bytes='';
         $cnt = $this->binary->read($bytes, 0);
         $this->assertEquals(392, $cnt);
-        $this->markTestIncomplete('TODO: check the resulting string when jr_cr_binary is working');
+        $this->assertEquals($this->binarystring, $bytes);
     }
 
     /** @expectedException PHPCR_InvalidArgumentException */
