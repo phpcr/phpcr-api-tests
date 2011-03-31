30e544c51ba703558215fcd8a26cd960d9290e9b
dbu <david.buchmann@liip.ch>
2011-03-31 14:22:59 +0200

    adding test for register node type from cnd

---
+++ tests/nodetype/ManipulationTests.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../inc/baseCase.php');
+
+/**
+ * Covering jcr-2.8.3 spec $19
+ *
+ * (only a few tests, lots is tested by unit tests)
+ */
+class Write_Manipulation_MoveMethodsTest extends jackalope_baseCase
+{
+
+    protected function setUp()
+    {
+        $this->renewSession();
+        parent::setUp();
+    }
+
+
+    /**
+     * @covers Jackalope\NodeTypeManager::registerNodeTypesCnd
+     */
+    public function testRegisterNodeTypesCnd()
+    {
+        $workspace = $this->sharedFixture['session']->getWorkspace();
+        $ntm = $workspace->getNodeTypeManager();
+
+        $cnd = "
+        <'phpcr'='http://www.doctrine-project.org/phpcr-odm'>
+         [phpcr:managed]
+          mixin
+          - phpcr:alias (string)
+          [phpcr:test]
+          mixin
+          - phpcr:prop (string)
+          ";
+        $types = $ntm->registerNodeTypesCnd($cnd);
+        $this->assertEquals(2, count($types), 'Wrong number of nodes registered');
+        list($name, $type) = each($types);
+        $this->assertEquals('phpcr:managed', $name);
+        $this->assertType('PHPCR\NodeType\NodeTypeDefinitionInterface', $type);
+        list($name, $type) = each($types);
+        $this->assertEquals('phpcr:test', $name);
+        $this->assertType('PHPCR\NodeType\NodeTypeDefinitionInterface', $type);
+        $props = $type->getDeclaredPropertyDefinitions();
+        $this->assertEquals(1, count($props), 'Wrong number of properties in phpcr:test');
+        $this->assertEquals('phpcr:prop', $props[0]->getName());
+
+        /* we could test if all options of cdn are properly translated, but that
+         * is jackrabbit code and tested over there.
+         * we just read the created nodes from the server. reading everything
+         * properly is to be tested in node type read tests.
+         */
+    }
+}
