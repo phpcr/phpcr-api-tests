608a4696f4cc4e6aa8bd465e5fb56146ce841413
Michele Orselli <mo@ideato.it>
2011-05-02 14:39:39 +0200

    added inc/autoload.php in order to simplify autoloading jackrabbit_importexport in symfony2

---
+++ inc/autoload.php
@@ -0,0 +1,8 @@
+<?php
+/* 
+ * An autoload array to enable autoloading in sf2 (jackrabbit_importexport is used
+ * in CoreBundle).
+ */
+return array(
+  'jackrabbit_importexport' => 'importexport.php'
+);
