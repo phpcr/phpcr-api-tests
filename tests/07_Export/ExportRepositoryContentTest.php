7ad23ac3d05ef435ae961302e7771e5476ab3c6f
dbu <david.buchmann@liip.ch>
2011-06-02 14:20:38 +0200

    adding fixtures for repository export and added some comments in read tests

--- tests/07_Export/ExportRepositoryContentTest.php
+++ tests/07_Export/ExportRepositoryContentTest.php
@@ -7,18 +7,39 @@ class Export_7_ExportRepositoryContentTest extends phpcr_suite_baseCase
     static public function setupBeforeClass()
     {
         parent::setupBeforeClass();
-        self::$staticSharedFixture['ie']->import('general/base');
+        self::$staticSharedFixture['ie']->import('07_Export/systemview');
     }
 
     public function testExportSystemView()
     {
+        $stream = fopen('php://memory', 'rwb+');
+        $this->sharedFixture['session']->exportSystemView('/tests_general_base', $stream, false, false);
+        rewind($stream);
+        $xml = stream_get_contents($stream);
+        $this->assertXmlStringEqualsXmlString(file_get_contents(__DIR__.'/../../fixtures/07_Export/systemview.xml'), $xml, true);
+    }
+
+    public function testExportDocumentView()
+    {
+        $stream = fopen('php://memory', 'rwb+');
+        $this->sharedFixture['session']->exportDocumentView('/tests_general_base', $stream, false, false);
+        rewind($stream);
+        $xml = stream_get_contents($stream);
+//var_dump($xml);
+        $this->assertXmlStringEqualsXmlString(file_get_contents(__DIR__.'/../../fixtures/07_Export/documentview.xml'), $xml, true);
+    }
+
+    public function testExportSystemViewSax()
+    {
+        $this->markTestSkipped('TODO: SAX ContentHandler');
         $xmlwriter = new XMLWriter();
         $this->assertTrue($xmlwriter->openMemory());
         $this->sharedFixture['session']->exportSystemView('/', $xmlwriter, false, false);
         echo $xmlwriter->outputMemory(true);
     }
-    public function testExportDocumentView()
+    public function testExportDocumentViewSax()
     {
+        $this->markTestSkipped('TODO: SAX ContentHandler');
         $xmlwriter = new XMLWriter();
         $this->assertTrue($xmlwriter->openMemory());
         $this->sharedFixture['session']->exportDocumentView('/', $xmlwriter, false, false);
