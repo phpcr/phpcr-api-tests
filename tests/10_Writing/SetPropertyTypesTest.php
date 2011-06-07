bc994667fd8bcb119544aef8ec59f554e2f9d969
Daniel Barsotti <daniel.barsotti@liip.ch>
2011-06-07 11:21:52 +0200

    Completed 10_Writing/SetPropertyTypesTest tests

--- tests/10_Writing/SetPropertyTypesTest.php
+++ tests/10_Writing/SetPropertyTypesTest.php
@@ -4,6 +4,10 @@ require_once(dirname(__FILE__) . '/../../inc/baseCase.php');
 /**
  * Testing whether the property correctly handles all types
  *
+ * For every test we do the assertions twice:
+ *   - Once after the property has been set in memory
+ *   - Once after renewing the session and reading the property from the backend
+ *
  * Covering jcr-2.8.3 spec $10.4.2
  */
 class Writing_10_SetPropertyTypesTest extends phpcr_suite_baseCase
@@ -36,10 +40,21 @@ class Writing_10_SetPropertyTypesTest extends phpcr_suite_baseCase
         $this->assertSame('10.6 test', $value->getString());
         $this->assertSame(10, $value->getLong());
         $this->assertEquals(\PHPCR\PropertyType::STRING, $value->getType());
+
+        $this->saveAndRenewSession();
+        $value = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/x');
+        $this->assertSame('10.6 test', $value->getString());
+        $this->assertSame(10, $value->getLong());
+        $this->assertEquals(\PHPCR\PropertyType::STRING, $value->getType());
     }
     public function testCreateValueBinary()
     {
         $bin = $this->node->setProperty('newBinary', 'foobar', PHPCR\PropertyType::BINARY);
+        $this->assertEquals(\PHPCR\PropertyType::BINARY, $bin->getType());
+        $this->assertEquals('foobar', stream_get_contents($bin->getBinary()));
+
+        $this->saveAndRenewSession();
+        $bin = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/newBinary');
         $this->assertEquals(\PHPCR\PropertyType::BINARY, $bin->getType());
         $this->assertEquals('foobar', stream_get_contents($bin->getBinary()));
     }
@@ -49,10 +64,23 @@ class Writing_10_SetPropertyTypesTest extends phpcr_suite_baseCase
         $this->assertSame('100', $value->getString());
         $this->assertSame(100, $value->getLong());
         $this->assertEquals(\PHPCR\PropertyType::LONG, $value->getType());
+
+        $this->saveAndRenewSession();
+        $value = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/x');
+        $this->assertSame('100', $value->getString());
+        $this->assertSame(100, $value->getLong());
+        $this->assertEquals(\PHPCR\PropertyType::LONG, $value->getType());
     }
     public function testCreateValueDouble()
     {
         $value = $this->node->setProperty('x', 10.6);
+        $this->assertSame('10.6', $value->getString());
+        $this->assertSame(10.6, $value->getDouble());
+        $this->assertSame(10, $value->getLong());
+        $this->assertEquals(\PHPCR\PropertyType::DOUBLE, $value->getType());
+
+        $this->saveAndRenewSession();
+        $value = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/x');
         $this->assertSame('10.6', $value->getString());
         $this->assertSame(10.6, $value->getDouble());
         $this->assertSame(10, $value->getLong());
@@ -65,6 +93,13 @@ class Writing_10_SetPropertyTypesTest extends phpcr_suite_baseCase
         $this->assertTrue($value->getBoolean(), 'boolean not true');
         $this->assertTrue($value->getString() == true, 'wrong string value'); //boolean converted to string must be true
         $this->assertSame(1, $value->getLong(), 'wrong integer value');
+
+        $this->saveAndRenewSession();
+        $value = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/x');
+        $this->assertEquals(\PHPCR\PropertyType::BOOLEAN, $value->getType(), 'wrong type');
+        $this->assertTrue($value->getBoolean(), 'boolean not true');
+        $this->assertTrue($value->getString() == true, 'wrong string value'); //boolean converted to string must be true
+        $this->assertSame(1, $value->getLong(), 'wrong integer value');
     }
     public function testCreateValueNode()
     {
@@ -72,11 +107,21 @@ class Writing_10_SetPropertyTypesTest extends phpcr_suite_baseCase
         $value = $this->node->setProperty('x', $node);
         $this->assertEquals(\PHPCR\PropertyType::REFERENCE, $value->getType(), 'wrong type');
         $this->assertEquals($node->getIdentifier(), $value->getString(), 'different uuid');
+
+        $this->saveAndRenewSession();
+        $value = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/x');
+        $this->assertEquals(\PHPCR\PropertyType::REFERENCE, $value->getType(), 'wrong type');
+        $this->assertEquals($node->getIdentifier(), $value->getString(), 'different uuid');
     }
     public function testCreateValueNodeWeak()
     {
         $node = $this->sharedFixture['session']->getRootNode()->getNode('tests_nodetype_base/multiValueProperty');
         $value = $this->node->setProperty('x', $node, \PHPCR\PropertyType::WEAKREFERENCE);
+        $this->assertEquals(\PHPCR\PropertyType::WEAKREFERENCE, $value->getType());
+        $this->assertEquals($node->getIdentifier(), $value->getString());
+
+        $this->saveAndRenewSession();
+        $value = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/x');
         $this->assertEquals(\PHPCR\PropertyType::WEAKREFERENCE, $value->getType());
         $this->assertEquals($node->getIdentifier(), $value->getString());
     }
@@ -101,11 +146,20 @@ class Writing_10_SetPropertyTypesTest extends phpcr_suite_baseCase
     {
         $value = $this->node->setProperty('x', 33, \PHPCR\PropertyType::STRING);
         $this->assertEquals(\PHPCR\PropertyType::STRING, $value->getType());
+
+        $this->saveAndRenewSession();
+        $value = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/x');
+        $this->assertEquals(\PHPCR\PropertyType::STRING, $value->getType());
     }
     public function testCreateValueDateType()
     {
         $time = time();
         $value = $this->node->setProperty('x', $time, \PHPCR\PropertyType::DATE);
+        $this->assertEquals(\PHPCR\PropertyType::DATE, $value->getType());
+        $this->assertEquals(date('Y-m-d\TH:i:s.000P', $time), $value->getString());
+
+        $this->saveAndRenewSession();
+        $value = $this->sharedFixture['session']->getProperty('/tests_nodetype_base/numberPropertyNode/jcr:content/x');
         $this->assertEquals(\PHPCR\PropertyType::DATE, $value->getType());
         $this->assertEquals(date('Y-m-d\TH:i:s.000P', $time), $value->getString());
     }
