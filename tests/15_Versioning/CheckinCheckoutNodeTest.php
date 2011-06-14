d9140c7875e49def57723c54f6e7fb62748de357
Henri Bergius <henri.bergius@iki.fi>
2011-06-14 11:02:26 +0300

    Minor CS fix

--- tests/15_Versioning/CheckinCheckoutNodeTest.php
+++ tests/15_Versioning/CheckinCheckoutNodeTest.php
@@ -19,8 +19,7 @@ class Versioning_15_CheckinCheckoutNodeTest extends phpcr_suite_baseCase
     {
         parent::setUp();
         $this->node = $this->sharedFixture['session']->getNode('/tests_version_base/versionable');
-        try
-        {
+        try {
             $this->vm = $this->sharedFixture['session']->getWorkspace()->getVersionManager();
         } catch (\PHPCR\UnSupportedRepositoryOperationException $e) {
             $this->markTestSkipped("Versioning not supported: " . $e->getMessage());
