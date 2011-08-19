6b1654ec731d2ccfa0ae0e0314b00626207ef974
dbu <david.buchmann@liip.ch>
2011-08-19 15:54:59 +0200

    adding tests for the session refresh

--- tests/10_Writing/CombinedManipulationsTest.php
+++ tests/10_Writing/CombinedManipulationsTest.php
@@ -10,6 +10,18 @@ use PHPCR\PropertyType as Type;
  */
 class CombinedManipulationsTest extends \PHPCR\Test\BaseCase
 {
+    static public function setupBeforeClass($fixtures = '10_Writing/combinedmanipulations')
+    {
+        parent::setupBeforeClass($fixtures);
+    }
+
+    public function setUp()
+    {
+        $this->renewSession(); // kill cache between tests
+        parent::setUp();
+        $this->assertInstanceOf('\PHPCR\NodeInterface', $this->node);
+    }
+
     /**
      * remove a node and then add a new one at the same path
      *
@@ -18,32 +30,260 @@ class CombinedManipulationsTest extends \PHPCR\Test\BaseCase
     public function testRemoveAndAdd()
     {
         $session = $this->sharedFixture['session'];
-        $basenode = $session->getNode('/tests_general_base');
-        $this->assertInstanceOf('PHPCR\NodeInterface', $basenode);
-        $node = $basenode->getNode('numberPropertyNode');
-        $this->assertInstanceOf('PHPCR\NodeInterface', $node);
+        $node = $this->node->getNode('child');
+        $path = $node->getPath();
         $this->assertInstanceOf('PHPCR\NodeType\NodeTypeInterface', $node->getPrimaryNodeType());
-        $this->assertSame('nt:file', $node->getPrimaryNodeType()->getName());
+        $this->assertSame('nt:unstructured', $node->getPrimaryNodeType()->getName());
 
         $node->remove();
 
-        $newnode = $basenode->addNode('numberPropertyNode', 'nt:folder');
+        $newnode = $this->node->addNode('child', 'nt:folder');
 
         $session = $this->saveAndRenewSession();
 
-        $node = $session->getNode('/tests_general_base/numberPropertyNode');
+
+        $node = $session->getNode($path);
         $this->assertInstanceOf('PHPCR\NodeInterface', $node);
         $this->assertSame('nt:folder', $node->getPrimaryNodeType()->getName());
         $this->assertFalse($node->hasNodes());
     }
 
     /*
-     * add more:
+     * TODO: add more combined manipulations:
      * move a not yet loaded node, then load it with the old path -> fail. with new path -> get it
      * same with moving child nodes not yet loaded and calling Node::getChildren. and loaded as well.
      * Test if order of write operations to backend is correct in larger batches. if i have
      * /some/path/parent/node and set the path of "parent" to /some/other/path/parent and in the same session change the path of node to /some/path/parent/something/node, result will depend on the order.
      * if you first move parent, then node, node ends up at the expected path.
      * if you first move node, then parent, node will end up in /some/other/path/parent/something/node, because a node is moved with all its children.
+     *
+     * what happens on save() for move /a/b/c, /a, remove /a/b? and what if we have /a/c and /a/b/c and want to remove /a/c, move /a/b/c, /a
      */
+
+    public function testSessionHasPendingChanges()
+    {
+        $session = $this->sharedFixture['session'];
+        $this->assertFalse($session->hasPendingChanges());
+        $this->node->setProperty('prop', "New");
+        $this->assertTrue($session->hasPendingChanges());
+    }
+
+    public function testSimpleSessionRefresh()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+
+        $node->setProperty('prop', 'New');
+        $this->assertEquals('New', $node->getPropertyValue('prop'));
+
+        $othersession = self::$loader->getSession();
+        $othernode = $othersession->getNode($node->getPath());
+        $othernode->setProperty('prop', 'Other');
+        $othernode->setProperty('newprop', 'Test');
+        $othersession->save();
+
+        $session->refresh(false);
+        $this->assertEquals('Other', $node->getPropertyValue('prop'));
+        $this->assertTrue($node->hasProperty('newprop'));
+        $this->assertEquals('Test', $node->getPropertyValue('newprop'));
+    }
+
+    public function testSimpleSessionRefreshKeepChanges()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+
+        $node->setProperty('prop', 'New');
+        $this->assertEquals('New', $node->getPropertyValue('prop'));
+
+        $othersession = self::$loader->getSession();
+        $othernode = $othersession->getNode($node->getPath());
+        $othernode->setProperty('prop', 'Other');
+        $othernode->setProperty('newprop', 'Test');
+        $othersession->save();
+
+        $session->refresh(true);
+        $this->assertEquals('New', $node->getPropertyValue('prop'));
+        $this->assertTrue($node->hasProperty('newprop'));
+        $this->assertEquals('Test', $node->getPropertyValue('newprop'));
+    }
+
+    public function testRemoveSessionRefresh()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+
+        $node->setProperty('prop', null);
+        $this->assertFalse($node->hasProperty('prop'));
+        $child = $node->getNode('child');
+        $child->remove();
+        $this->assertFalse($node->hasNode('child'));
+        $this->assertFalse($session->nodeExists($node->getPath() . '/child'));
+
+        $session->refresh(false);
+        $this->assertEquals('Old', $node->getPropertyValue('prop'));
+        $this->assertTrue($node->hasNode('child'));
+        $this->assertTrue($session->nodeExists($node->getPath() . '/child'));
+        $this->assertSame($child, $session->getNode($node->getPath() . '/child'));
+    }
+
+    public function testRemoveSessionRefreshKeepChanges()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+
+        $node->setProperty('prop', null);
+        $this->assertFalse($node->hasProperty('prop'));
+        $child = $node->getNode('child');
+        $child->remove();
+        $this->assertFalse($node->hasNode('child'));
+        $this->assertFalse($session->nodeExists($node->getPath() . '/child'));
+
+        $session->refresh(true);
+        $this->assertEquals('Old', $node->getPropertyValue('prop'));
+        $this->assertFalse($node->hasNode('child'));
+        $this->assertFalse($session->nodeExists($node->getPath() . '/child'));
+    }
+
+    /**
+     * remove a child node and a property in a different session. should
+     * disappear on refresh, even if we want to keep changes
+     */
+    public function testRemoveOtherSessionRefreshKeepChanges()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+
+        $node->setProperty('prop', null);
+        $this->assertFalse($node->hasProperty('prop'));
+
+        $session->refresh(false);
+        $this->assertEquals('Old', $node->getPropertyValue('prop'));
+    }
+
+    public function testMoveSessionRefresh()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+        $child = $node->getNode('src/child');
+
+        $session->move($node->getPath() . '/src/child', $node->getPath() . '/target/childnew');
+
+        $this->assertFalse($session->nodeExists($node->getPath() . '/src/child'));
+        $this->assertTrue($session->nodeExists($node->getPath() . '/target/childnew'));
+
+        $session->refresh(false);
+
+        $this->assertTrue($session->nodeExists($node->getPath() . '/src/child'));
+        $this->assertFalse($session->nodeExists($node->getPath() . '/target/childnew'));
+        $this->assertEquals($node->getPath() . '/src/child', $child->getPath());
+        $src = $node->getNode('src');
+        $this->assertTrue($src->hasNode('child'));
+        $this->assertSame($child, $src->getNode('child'));
+        $target = $node->getNode('target');
+        $this->assertFalse($target->hasNode('childnew'));
+    }
+
+    public function testMoveSessionRefreshKeepChanges()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+        $child = $node->getNode('src/child');
+
+        $session->move($node->getPath() . '/src/child', $node->getPath() . '/target/childnew');
+
+        $this->assertFalse($session->nodeExists($node->getPath() . '/src/child'));
+        $this->assertTrue($session->nodeExists($node->getPath() . '/target/childnew'));
+
+        $session->refresh(true);
+
+        $this->assertFalse($session->nodeExists($node->getPath() . '/src/child'));
+        $this->assertTrue($session->nodeExists($node->getPath() . '/target/childnew'));
+        $this->assertEquals($node->getPath() . '/target/childnew', $child->getPath());
+        $src = $node->getNode('src');
+        $this->assertFalse($src->hasNode('child'));
+        $target = $node->getNode('target');
+        $this->assertTrue($target->hasNode('childnew'));
+        $this->assertSame($child, $target->getNode('childnew'));
+    }
+
+    public function testAddSessionRefresh()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+        $child = $node->addNode('child');
+        $property = $node->setProperty('prop', 'Test');
+
+        $session->refresh(false);
+
+        $this->assertFalse($node->hasNode('child'));
+        $this->assertFalse($node->hasProperty('prop'));
+
+        $this->assertFalse($session->nodeExists($node->getPath().'/child'));
+        $this->assertFalse($session->propertyExists($node->getPath().'/prop'));
+
+        $this->setExpectedException('\PHPCR\PathNotFoundException');
+        $node->getNode('child');
+    }
+
+    public function testAddSessionRefreshKeepChanges()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+        $child = $node->addNode('child');
+        $property = $node->setProperty('prop', 'Test');
+
+        $session->refresh(true);
+
+        $this->assertTrue($node->hasNode('child'));
+        $this->assertTrue($node->hasProperty('prop'));
+
+        $this->assertTrue($session->nodeExists($node->getPath().'/child'));
+        $this->assertTrue($session->propertyExists($node->getPath().'/prop'));
+
+        $node->getNode('child');
+    }
+
+    public function testNodeRefresh()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+
+        $child = $node->addNode('child');
+        $property = $node->setProperty('other', 'Test');
+        $existingProperty = $node->setProperty('prop', 'New');
+
+        $node->refresh(false);
+
+        $this->assertSame($existingProperty, $node->getProperty('prop'));
+        $this->assertSame('Old', $existingProperty->getValue());
+        $this->assertFalse($node->hasNode('child'));
+        $this->assertFalse($node->hasProperty('other'));
+        $this->assertFalse($session->nodeExists($node->getPath().'/child'));
+        $this->assertFalse($session->propertyExists($node->getPath().'/other'));
+
+        $this->setExpectedException('\PHPCR\PathNotFoundException');
+        $node->getPropertyValue('other');
+    }
+
+    public function testNodeRefreshKeepChanges()
+    {
+        $session = $this->sharedFixture['session'];
+        $node = $this->node;
+
+        $child = $node->addNode('child');
+        $property = $node->setProperty('other', 'Test');
+        $existingProperty = $node->setProperty('prop', 'New');
+
+        $node->refresh(true);
+
+        $this->assertSame($existingProperty, $node->getProperty('prop'));
+        $this->assertSame('New', $existingProperty->getValue());
+        $this->assertTrue($node->hasNode('child'));
+        $this->assertTrue($node->hasProperty('other'));
+        $this->assertTrue($session->nodeExists($node->getPath().'/child'));
+        $this->assertTrue($session->propertyExists($node->getPath().'/other'));
+
+        $this->assertSame($property, $node->getProperty('other'));
+    }
 }
