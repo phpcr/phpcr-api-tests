0369ea336017a087d13d8a48717358115ef6b74e
dbu <david.buchmann@liip.ch>
2012-03-20 20:04:36 +0100

    testing system view import

--- tests/11_Import/ImportRepositoryContentTest.php
+++ tests/11_Import/ImportRepositoryContentTest.php
@@ -13,8 +13,18 @@ class ImportRepositoryContentTest extends \PHPCR\Test\BaseCase
 
     public function testImportXMLSystem()
     {
-        $this->sharedFixture['session']->importXML('/', __DIR__.'/../../fixtures/general/base.xml', 0);
+        $session = $this->sharedFixture['session'];
+        $session->importXML('/', __DIR__.'/../../fixtures/general/base.xml', 0);
 
+        $this->assertTrue($session->nodeExists('/tests_general_base'));
+        $this->assertTrue($session->propertyExists('/tests_general_base/idExample/jcr:content/weakreference_target/jcr:uuid'));
+        $uuid = $session->getProperty('/tests_general_base/idExample/jcr:content/weakreference_target/jcr:uuid');
+        $this->assertInstanceOf('PHPCR\PropertyInterface', $uuid);
+        $this->assertEquals('13543fc6-1abf-4708-bfcc-e49511754b40', $uuid->getString());
+
+        $ref = $session->getProperty('/tests_general_base/idExample/jcr:content/weakreference_source1/ref1');
+        $this->assertEquals(\PHPCR\PropertyType::WEAKREFERENCE, $ref->getType());
+        $this->assertEquals('13543fc6-1abf-4708-bfcc-e49511754b40', $ref->getString());
     }
 
     public function testImportXMLDocument()
