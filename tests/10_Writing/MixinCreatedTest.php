66030c38c909710398fa7c7581fb73472d05b7e9
David Buchmann <david.buchmann@liip.ch>
2012-07-11 12:11:07 +0200

    adding test about autocreated date property

---
+++ tests/10_Writing/MixinCreatedTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace PHPCR\Tests\Writing;
+
+require_once(__DIR__ . '/../../inc/BaseCase.php');
+
+/**
+ * Testing that mix:referenceable nodes references work correctly
+ *
+ * Covering jcr-2.8.3 spec $10.10.3
+ */
+class MixinCreatedTest extends \PHPCR\Test\BaseCase
+{
+    public function setUp()
+    {
+        $this->renewSession(); // discard changes
+    }
+
+    /**
+     * Test that a node with newly set mix:referenceable type can be referenced
+     */
+    public function testCreationNode()
+    {
+        $path = '/tests_general_base/idExample/jcr:content';
+        /** @var $node \PHPCR\NodeInterface */
+        $node = $this->sharedFixture['session']->getNode($path);
+        $child = $node->addNode('test');
+        $path .= '/test';
+        $this->assertEquals($path, $child->getPath());
+        $child->addMixin('mix:created');
+
+        $this->sharedFixture['session']->save();
+
+        $this->assertTrue($child->isNodeType('mix:created'));
+        $this->assertTrue($child->hasProperty('jcr:created'));
+        $date = $child->getPropertyValue('jcr:created');
+        $this->assertInstanceOf('DateTime', $date);
+        /** @var $date \DateTime */
+        $diff = time() - $date->getTimestamp();
+        $this->assertTrue($diff < 60*10, "jcr:created should be current date as fixture was just imported: ".$date->format('c'));
+
+        // Re-read the node to be sure things got properly saved
+        $this->renewSession();
+        $child = $this->sharedFixture['session']->getNode($path);
+
+        $this->assertTrue($child->isNodeType('mix:created'));
+        $this->assertTrue($child->hasProperty('jcr:created'));
+        $date = $child->getPropertyValue('jcr:created');
+        $this->assertInstanceOf('DateTime', $date);
+        $diff = time() - $date->getTimestamp();
+        $this->assertTrue($diff < 60*10, "jcr:created should be current date as fixture was just imported: ".$date->format('c'));
+    }
+}
