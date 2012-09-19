c992ab092556e2a72f90fefee90c5c4744cda559
Daniel Barsotti <daniel.barsotti@liip.ch>
2012-09-19 13:43:54 +0200

    Completed the test for the CND parser

--- tests/19_NodeTypeManagement/CndParserTest.php
+++ tests/19_NodeTypeManagement/CndParserTest.php
@@ -10,16 +10,18 @@ use PHPCR\Util\CND\Helper\NodeTypeGenerator,
     PHPCR\Util\CND\Reader\BufferReader,
     PHPCR\Util\CND\Parser\CndParser,
     PHPCR\Util\CND\Scanner\GenericScanner,
-    PHPCR\Util\CND\Scanner\Context;
+    PHPCR\Util\CND\Scanner\Context,
+    PHPCR\PropertyType,
+    PHPCR\Version\OnParentVersionAction;
 
 /**
  * Test for PHPCR\Util\QOM\QomToSql2QueryConverter
  */
 class CndParserTest extends \PHPCR\Test\BaseCase
 {
-  function testGenerator()
-  {
-    $cnd = <<<EOT
+    function testGenerator()
+    {
+        $cnd = <<<EOT
 /*  An example node type definition */
 <ns ='http://namespace.com/ns'>
 [ns:NodeType] > ns:ParentType1, ns:ParentType2
@@ -34,31 +36,47 @@ class CndParserTest extends \PHPCR\Test\BaseCase
     mandatory autocreated protected VERSION
 EOT;
 
-    $reader = new BufferReader($cnd);
-    $scanner = new GenericScanner(new Context\DefaultScannerContextWithoutSpacesAndComments());
-    $queue = $scanner->scan($reader);
+        $reader = new BufferReader($cnd);
+        $scanner = new GenericScanner(new Context\DefaultScannerContextWithoutSpacesAndComments());
+        $queue = $scanner->scan($reader);
 
-    //define('DEBUG', true);
+        //define('DEBUG', true);
 
-    $parser = new CndParser($queue);
+        $parser = new CndParser($queue);
 
-    $generator = new NodeTypeGenerator(
-      $this->sharedFixture['session']->getWorkspace(),
-      $parser->parse()
-    );
+        $generator = new NodeTypeGenerator(
+            $this->sharedFixture['session']->getWorkspace(),
+            $parser->parse()
+        );
 
-    $res = $generator->generate();
-    $def = reset($res['nodeTypes']);
+        $res = $generator->generate();
+        $def = reset($res['nodeTypes']);
 
-    $this->assertEquals(array('ns' => 'http://namespace.com/ns'), $res['namespaces']);
+        $this->assertEquals(array('ns' => 'http://namespace.com/ns'), $res['namespaces']);
 
-    $this->assertInstanceOf('\PHPCR\NodeType\NodeTypeTemplateInterface', $def);
-    $this->assertEquals('ns:NodeType', $def->getName());
-    $this->assertEquals(array('ns:ParentType1', 'ns:ParentType2'), $def->getDeclaredSuperTypeNames());
-    $this->assertTrue($def->hasOrderableChildNodes());
-    $this->assertTrue($def->isMixin());
-    $this->assertFalse($def->isQueryable());
-    $this->assertFalse($def->isAbstract());
-  }
+        $this->assertInstanceOf('\PHPCR\NodeType\NodeTypeTemplateInterface', $def);
+        $this->assertEquals('ns:NodeType', $def->getName());
+        $this->assertEquals(array('ns:ParentType1', 'ns:ParentType2'), $def->getDeclaredSuperTypeNames());
+        $this->assertTrue($def->hasOrderableChildNodes());
+        $this->assertTrue($def->isMixin());
+        $this->assertFalse($def->isQueryable());
+        $this->assertFalse($def->isAbstract());
+        $this->assertEquals(1, count($def->getPropertyDefinitionTemplates()));
+
+        $prop = $def->getPropertyDefinitionTemplates()->getIterator()->current();
+
+        $this->assertEquals('ex:property', $prop->getName());
+        $this->assertEquals(PropertyType::STRING, $prop->getRequiredType());
+        $this->assertEquals(array('default1', 'default2'), $prop->getDefaultValues());
+        $this->assertEquals(array('constraint1', 'constraint2'), $prop->getValueConstraints());
+        $this->assertTrue($prop->isAutoCreated());
+        $this->assertTrue($prop->isMandatory());
+        $this->assertTrue($prop->isProtected());
+        $this->assertTrue($prop->isMultiple());
+        $this->assertEquals(OnParentVersionAction::VERSION, $prop->getOnParentVersion());
+        $this->assertEquals(array(), $prop->getAvailableQueryOperators());
+        $this->assertTrue($prop->isFullTextSearchable()); // True because there was no "nofulltext" attribute
+        $this->assertTrue($prop->isQueryOrderable());     // True because there was no "noqueryorder" attribute
+    }
 
 }
