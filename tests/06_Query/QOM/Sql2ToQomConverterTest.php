aa8e78345c6157359bcd01f0e4b1a742c8551c02
David Buchmann <david.buchmann@liip.ch>
2013-07-25 09:00:49 +0200

    qom always uses a selector name

--- tests/06_Query/QOM/Sql2ToQomConverterTest.php
+++ tests/06_Query/QOM/Sql2ToQomConverterTest.php
@@ -39,24 +39,19 @@ class Sql2ToQomConverterTest extends \PHPCR\Test\BaseCase
         $this->assertInstanceOf('\PHPCR\Query\QOM\QueryObjectModelInterface', $query);
         $this->assertNull($query->getConstraint());
         $this->assertEmpty($query->getOrderings());
-        $this->assertInstanceOf('\PHPCR\Query\QOM\SourceInterface', $query->getSource());
+        $this->assertInstanceOf('\PHPCR\Query\QOM\SelectorInterface', $query->getSource());
         $this->assertEquals('nt:unstructured', $query->getSource()->getNodeTypeName());
 
         $cols = $query->getColumns();
         $this->assertTrue(is_array($cols));
-        $this->assertEquals(3, count($cols));
+        $this->assertEquals(2, count($cols));
 
+        $this->assertEquals('u', $cols[0]->getselectorName());
         $this->assertEquals('prop1', $cols[0]->getPropertyName());
-        $this->assertNull($cols[0]->getselectorName());
-        $this->assertNull($cols[0]->getColumnName());
-
+        $this->assertEquals('col1', $cols[0]->getColumnName());
+        $this->assertEquals('u', $cols[1]->getselectorName());
         $this->assertEquals('prop2', $cols[1]->getPropertyName());
-        $this->assertNull($cols[1]->getselectorName());
-        $this->assertEquals('col2', $cols[1]->getColumnName());
-
-        $this->assertEquals('prop3', $cols[2]->getPropertyName());
-        $this->assertEquals('sel3', $cols[2]->getSelectorName());
-        $this->assertEquals('col3', $cols[2]->getColumnName());
+        $this->assertEquals('prop2', $cols[1]->getColumnName());
     }
 
     public function testQueries()
