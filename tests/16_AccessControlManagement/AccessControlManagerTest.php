3e154f8a2e8b3fe8521ba9a180096db821faf4f8
Lukas Kahwe Smith <smith@pooteeweet.org>
2015-02-10 13:46:16 +0100

    WIP setPolicy()

--- tests/16_AccessControlManagement/AccessControlManagerTest.php
+++ tests/16_AccessControlManagement/AccessControlManagerTest.php
@@ -1,6 +1,9 @@
 <?php
 namespace PHPCR\Tests\AccessControlManagement;
 
+use Jackalope\Security\AccessControlList;
+use Jackalope\Security\AccessControlPolicy;
+
 require_once(__DIR__ . '/../../inc/BaseCase.php');
 
 class AccessControlManagerTest extends \PHPCR\Test\BaseCase
@@ -13,4 +16,14 @@ class AccessControlManagerTest extends \PHPCR\Test\BaseCase
 //        var_dump($manager->getPolicies('/')->getAccessControlEntries());
     }
 
+    public function testWriteAcl()
+    {
+        $manager = $this->session->getAccessControlManager();
+
+        $path = '/foo';
+        $list = $manager->getApplicablePolicies('/foo');
+        $policy = reset($list);
+        $manager->setPolicy($path, $policy);
+        $this->session->save();
+    }
 }
