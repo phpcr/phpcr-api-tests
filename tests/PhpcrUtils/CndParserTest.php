6ca175b8048fd492088022cf3e2188236fc34706
Pavel Vondrasek <pavel@appio.cz>
2017-01-22 14:49:24 +0100

    skip test that hangs in PHP 7 until we can fix the cnd parser bug

--- tests/PhpcrUtils/CndParserTest.php
+++ tests/PhpcrUtils/CndParserTest.php
@@ -111,6 +111,14 @@ EOT;
      */
     public function testScannerErrorComment()
     {
+        /**
+         * Remove this PHP7 only condition after PHPCR-Utils #168
+         * (https://github.com/phpcr/phpcr-utils/issues/168) is closed
+         */
+        if (PHP_MAJOR_VERSION >= 7) {
+            $this->markTestSkipped();
+        }
+
         $this->expectException(ScannerException::class);
 
         $cnd = <<<EOT
